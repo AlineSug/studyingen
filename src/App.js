6f22ba7e533a1d39113376e2520df0e114faaeec
Aline Sugisaki <aline_spinelly@hotmail.com>
2025-06-02 15:40:46 +0900

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import logo from "./logolearning.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Top />
+      <FlashCard />
     </div>
   );
 }
 
-export default App;
+function Top() {
+  return (
+    <div className="top">
+      <img src={logo} alt="Logo" style={{ height: 150 }} />
+      <h1>Conhecendo frases nativas usadas</h1>
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "You Nailed it!",
+    answer: "Voce acertou(conseguiu)",
+  },
+  {
+    id: 7336,
+    question: "To seized ",
+    answer: "Conquistar/Agarrar/Se apoderar",
+  },
+  {
+    id: 8832,
+    question: "Made a pass at me",
+    answer: "Dar em cima/ investida/ flertar",
+  },
+  { id: 2233, question: "Double Standard", answer: "Dois pesos duas medidas" },
+];
+
+function FlashCard() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
